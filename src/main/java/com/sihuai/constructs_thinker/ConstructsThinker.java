4138e31b2c7ee16f6ea676c19fee02c779a880a6
Sihuai2412 <139676314+Sihuai2412@users.noreply.github.com>
2024-02-18 16:17:10 +0800

    Add files via upload

---
+++ src/main/java/com/sihuai/constructs_thinker/ConstructsThinker.java
@@ -0,0 +1,30 @@
+package com.sihuai.constructs_thinker;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+
+@Mod("constructs_thinker")
+public class ConstructsThinker {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public static final String MOD_ID = "constructs_thinker";
+}
